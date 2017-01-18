678e2125579b21ca9001d0358a19c4152b306cfe
Jack Rans <jwrans@gmail.com>
2017-01-18 08:40:10 +0000

    :bug: remove custom gulp tasks and commit up minified files with asset links modified :see_no_evil:

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,35 +2,13 @@
 
 var gulp = require('gulp');
 var RSBundler = require('@redsift/redsift-bundler');
-var download = require('gulp-download');
-var unzip = require('gulp-unzip');
-var replace = require('gulp-replace');
 
 // The definitions for this Sift's bundles
 var bundles = require('./bundle.config.js');
 
 gulp.task('bundle-js', RSBundler.loadTask(gulp, 'bundle-js', bundles('js')));
 gulp.task('bundle-css', RSBundler.loadTask(gulp, 'bundle-css', bundles('css')));
-gulp.task('download-chessboard', function() {
-  console.log('downloading chessboardjs');
-  download('http://chessboardjs.com/releases/0.3.0/chessboardjs-0.3.0.zip')
-    .pipe(unzip())
-    .pipe(gulp.dest('public/chessboardjs-0.3.0/'));
-});
 
-gulp.task('correct-image-urls', function () {
-  gulp.src([
-    'public/chessboardjs-0.3.0/js/chessboard-0.3.0.js'
-  ])
-    .pipe(replace('    cfg.pieceTheme = \'img/chesspieces/wikipedia/{piece}.png\';', '    cfg.pieceTheme = \'chessboardjs-0.3.0/img/chesspieces/wikipedia/{piece}.png\';'))
-    .pipe(gulp.dest('public/chessboardjs-0.3.0/js'));
-  gulp.src([
-    'public/chessboardjs-0.3.0/js/chessboard-0.3.0.min.js'
-  ])
-    .pipe(replace('b.pieceTheme="img/chesspieces/wikipedia/{piece}.png";', 'b.pieceTheme="chessboardjs-0.3.0/img/chesspieces/wikipedia/{piece}.png";'))
-    .pipe(gulp.dest('public/chessboardjs-0.3.0/js'));
-});
-
-gulp.task('default', ['download-chessboard', 'correct-image-urls', 'bundle-js', 'bundle-css'], function () {
+gulp.task('default', ['bundle-js', 'bundle-css'], function () {
   console.log('Bundling complete');
 });
