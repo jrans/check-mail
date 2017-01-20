463658da62c9e7f62dc55985af8c0f51cfcf5d02
Jack Rans <jwrans@gmail.com>
2017-01-20 08:14:50 +0000

    :heavy_plus_sign: Add client side feature validation and find move for them to copy into email

--- frontend/src/scripts/detail.js
+++ frontend/src/scripts/detail.js
@@ -1,36 +1,65 @@
 /* global ChessBoard */
 import { SiftView, registerSiftView } from '@redsift/sift-sdk-web';
+import Chess from 'chess.js';
 
 export default class DetailView extends SiftView {
+
   constructor() {
-    // You have to call the super() method to initialize the base class.
     super();
-    this.controller.subscribe('recalc', this.recalc.bind(this));
+    [
+      'recalc','onDrop','onDragStart','onSnapEnd', 'resetPosition'
+    ].forEach(method => this[method] = this[method].bind(this));
+    this.controller.subscribe('recalc', this.recalc);
+    this.game = new Chess();
+    this.plannedMove = document.getElementsByClassName('detail--planned')[0];
+    this.resetButton = document.getElementsByClassName('detail--reset')[0];
+    this.board = new ChessBoard('detail--board', {
+      draggable: true,
+      dropOffBoard: 'trash',
+      onDragStart: this.onDragStart,
+      onDrop: this.onDrop,
+      onSnapEnd: this.onSnapEnd
+    });
+    this.resetButton.onclick = this.resetPosition;
   }
 
-  presentView(got) {
+  presentView (got) {
     this.updateView(got.data);
-    document.getElementById('detail-reset').onclick = this.resetPosition.bind(this);
   }
 
-  recalc(got) {
+  recalc (got) {
     this.updateView(got);
   }
 
-  updateView(position){
+  updateView (position){
     if (position) {
-      this.chessBoard = ChessBoard('detail-board', {
-        position,
-        draggable: true,
-        dropOffBoard: 'trash',
-        onDrop: console.log
-      });
       this.startPosition = position;
+      this.resetPosition();
     }
   }
 
-  resetPosition() {
-    this.chessBoard.position(this.startPosition);
+  resetPosition () {
+    this.game.load(this.startPosition);
+    this.board.position(this.startPosition);
+    this.plannedMove.innerText = '';
+  }
+
+  onDragStart (_, piece) {
+    return !this.game.game_over() && piece.indexOf(this.game.turn()) > -1;
+  }
+
+  onDrop (from, to) {
+    const move = this.game.move({from, to, promotion: 'q'});
+    if (move === null) return 'snapback';
+    this.plannedMove.innerText = this.plannedMove.innerText === ''
+      ? `[ ${move.san} ]`
+      : this.plannedMove.innerText
+    ;
+    return;
+  }
+
+  onSnapEnd () {
+    this.board.position(this.game.fen());
   }
 }
 
